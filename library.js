9ef981dc76d1f93aedcfb09f8dccdbb084fa8450
anneKay <nwannaulk@gmail.com>
2017-01-09 16:23:47 +0100

    my dataTypes fuction

--- library.js
+++ library.js
@@ -1,18 +1,39 @@
-module.exports = {
- 
- function: findMaxMin(n) {
-  var myArr = [];
-  myMax = a[0];
-  myMin = a[0];
-  for(var i = 0; i < n.length; i++) {
-    if (n[i] > myMax) {
-      myMax = n[i];
+var dataTypes = function(val) {
+  var callback = function(arg) {
+    if (arg === true) {
+    return "called callback";
     }
-    else if (n[i] < myMin) {
-      myMin = n[i];
+  };
+  
+  if (val === null || val === undefined) {
+    return "no value";
+  
+  } else if (val === false){
+    return false;
+  
+  } else if (val === true){
+    return true;
+  
+  } else if (typeof(val) === 'number') {
+    if (val < 100) {
+      return "less than 100";
+     
+    } else if (val === 100) {
+      return "equal to 100" ;
+      
+    } else if (val > 100) {
+    return "greater than 100";
     }
+    
+  } else if (typeof(val) === 'string') {
+    return val.length;
+  
+  } else if (Array.isArray(val) === true) {
+    return val[2];
+    
+  } else if (typeof(val) === 'function'){
+  return callback(true);
+  } else {
+    return undefined;
   }
-  myArr = [myMin, myMax]
-  return myArr;
 }
-
